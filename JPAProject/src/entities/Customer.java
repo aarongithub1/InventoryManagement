2bcd90709dc28db6c2fc00abc22b62e8d3676abf
falcon <aaron.skilldistill@gmail.com>
2018-03-26 17:17:02 +0700

    complete Customer class

--- JPAProject/src/entities/Customer.java
+++ JPAProject/src/entities/Customer.java
@@ -15,14 +15,93 @@ public class Customer {
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
 	
-	private String username;
+	private String name;
 	private String email;
+	private String password;
 	
 	@Column(name="shipping_address")
 	private String shippingAddress;
 	@Column(name="billing_address")
 	private String billingAddress;
 	
-	private int cartId;
+	// GETTERS AND SETTERS
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String getShippingAddress() {
+		return shippingAddress;
+	}
+	public void setShippingAddress(String shippingAddress) {
+		this.shippingAddress = shippingAddress;
+	}
+	
+	public String getBillingAddress() {
+		return billingAddress;
+	}
+	public void setBillingAddress(String billingAddress) {
+		this.billingAddress = billingAddress;
+	}
+	
+	//toString
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Customer [id=");
+		builder.append(id);
+		builder.append(", Name=");
+		builder.append(name);
+		builder.append(", email=");
+		builder.append(email);
+		builder.append(", password=");
+		builder.append(password);
+		builder.append(", shipping_address=");
+		builder.append(shippingAddress);
+		builder.append(", billing_address=");
+		builder.append(billingAddress);
+		builder.append("]");
+		return builder.toString();
+	}
+	
+	
+	
 	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
