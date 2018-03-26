bb450060578c05e2258928a1cc853d1263fe28eb
falcon <aaron.skilldistill@gmail.com>
2018-03-26 14:30:35 +0700

    reconfigure database schema

--- JPAProject/src/entities/Customer.java
+++ JPAProject/src/entities/Customer.java
@@ -5,6 +5,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -23,6 +25,26 @@ public class Customer {
 	@Column(name="billing_address")
 	private String billingAddress;
 	
-	private int cartId;
+	@OneToOne
+	@JoinColumn(name="cart_id")
+	private Cart cart;
+	
 	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
