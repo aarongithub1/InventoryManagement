89ddcd58441085b5c4d9c2f9688916b88d026dfb
falcon <aaron.skilldistill@gmail.com>
2018-03-26 16:39:45 +0700

    fix database and revert

--- inventorymanagementdb.sql
+++ inventorymanagementdb.sql
@@ -21,53 +21,20 @@ USE `inventorymanagementdb` ;
 DROP TABLE IF EXISTS `inventorymanagementdb`.`customer` ;
 
 CREATE TABLE IF NOT EXISTS `inventorymanagementdb`.`customer` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(255) NULL,
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NULL DEFAULT NULL,
   `email` VARCHAR(255) NOT NULL,
   `password` VARCHAR(255) NOT NULL,
-  `shipping_address` VARCHAR(255) NULL,
-  `billing_address` VARCHAR(255) NULL,
-  `cart_id` INT NULL,
+  `shipping_address` VARCHAR(255) NULL DEFAULT NULL,
+  `billing_address` VARCHAR(255) NULL DEFAULT NULL,
   PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+ENGINE = InnoDB
+AUTO_INCREMENT = 3
+DEFAULT CHARACTER SET = utf8;
 
 CREATE UNIQUE INDEX `email_UNIQUE` ON `inventorymanagementdb`.`customer` (`email` ASC);
 
 CREATE UNIQUE INDEX `id_UNIQUE` ON `inventorymanagementdb`.`customer` (`id` ASC);
-
-
--- -----------------------------------------------------
--- Table `inventorymanagementdb`.`product`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `inventorymanagementdb`.`product` ;
-
-CREATE TABLE IF NOT EXISTS `inventorymanagementdb`.`product` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(255) NULL,
-  `quantity` INT(11) NULL,
-  `available` TINYINT(1) NULL,
-  `price` DECIMAL(10,2) NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
-
-CREATE UNIQUE INDEX `id_UNIQUE` ON `inventorymanagementdb`.`product` (`id` ASC);
-
-
--- -----------------------------------------------------
--- Table `inventorymanagementdb`.`staff`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `inventorymanagementdb`.`staff` ;
-
-CREATE TABLE IF NOT EXISTS `inventorymanagementdb`.`staff` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(255) NULL,
-  `picture` VARCHAR(255) NULL,
-  `bio` VARCHAR(2000) NULL,
-  `position` VARCHAR(255) NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
-
-CREATE UNIQUE INDEX `id_UNIQUE` ON `inventorymanagementdb`.`staff` (`id` ASC);
 
 
 -- -----------------------------------------------------
@@ -76,23 +43,46 @@ CREATE UNIQUE INDEX `id_UNIQUE` ON `inventorymanagementdb`.`staff` (`id` ASC);
 DROP TABLE IF EXISTS `inventorymanagementdb`.`order` ;
 
 CREATE TABLE IF NOT EXISTS `inventorymanagementdb`.`order` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `shipping_address` VARCHAR(255) NULL,
-  `billing_address` VARCHAR(255) NULL,
-  `total_price` DECIMAL(10,2) NULL,
-  `date_created` DATETIME NULL,
-  `date_shipped` DATETIME NULL,
-  `order_status` VARCHAR(45) NULL,
-  `customer_id` INT NOT NULL,
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `shipping_address` VARCHAR(255) NULL DEFAULT NULL,
+  `billing_address` VARCHAR(255) NULL DEFAULT NULL,
+  `total_price` DECIMAL(10,2) NULL DEFAULT NULL,
+  `date_created` DATETIME NULL DEFAULT NULL,
+  `date_shipped` DATETIME NULL DEFAULT NULL,
+  `order_status` VARCHAR(45) NULL DEFAULT NULL,
+  `customer_id` INT(11) NOT NULL,
   PRIMARY KEY (`id`),
   CONSTRAINT `fk_order_customer`
     FOREIGN KEY (`customer_id`)
     REFERENCES `inventorymanagementdb`.`customer` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+AUTO_INCREMENT = 3
+DEFAULT CHARACTER SET = utf8;
 
 CREATE UNIQUE INDEX `idorder_UNIQUE` ON `inventorymanagementdb`.`order` (`id` ASC);
+
+CREATE INDEX `fk_order_customer` ON `inventorymanagementdb`.`order` (`customer_id` ASC);
+
+
+-- -----------------------------------------------------
+-- Table `inventorymanagementdb`.`product`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `inventorymanagementdb`.`product` ;
+
+CREATE TABLE IF NOT EXISTS `inventorymanagementdb`.`product` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NULL DEFAULT NULL,
+  `quantity` INT(11) NULL DEFAULT NULL,
+  `available` TINYINT(1) NULL DEFAULT NULL,
+  `price` DECIMAL(10,2) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 4
+DEFAULT CHARACTER SET = utf8;
+
+CREATE UNIQUE INDEX `id_UNIQUE` ON `inventorymanagementdb`.`product` (`id` ASC);
 
 
 -- -----------------------------------------------------
@@ -101,9 +91,9 @@ CREATE UNIQUE INDEX `idorder_UNIQUE` ON `inventorymanagementdb`.`order` (`id` AS
 DROP TABLE IF EXISTS `inventorymanagementdb`.`order_product` ;
 
 CREATE TABLE IF NOT EXISTS `inventorymanagementdb`.`order_product` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `order_id` INT NOT NULL,
-  `product_id` INT NOT NULL,
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `order_id` INT(11) NULL DEFAULT NULL,
+  `product_id` INT(11) NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   CONSTRAINT `fk_order_product`
     FOREIGN KEY (`order_id`)
@@ -115,13 +105,34 @@ CREATE TABLE IF NOT EXISTS `inventorymanagementdb`.`order_product` (
     REFERENCES `inventorymanagementdb`.`product` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+AUTO_INCREMENT = 7
+DEFAULT CHARACTER SET = utf8;
+
+CREATE UNIQUE INDEX `id_UNIQUE` ON `inventorymanagementdb`.`order_product` (`id` ASC);
 
 CREATE INDEX `fk_product_order_idx` ON `inventorymanagementdb`.`order_product` (`product_id` ASC);
 
 CREATE INDEX `fk_order_product_idx` ON `inventorymanagementdb`.`order_product` (`order_id` ASC);
 
-CREATE UNIQUE INDEX `id_UNIQUE` ON `inventorymanagementdb`.`order_product` (`id` ASC);
+
+-- -----------------------------------------------------
+-- Table `inventorymanagementdb`.`staff`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `inventorymanagementdb`.`staff` ;
+
+CREATE TABLE IF NOT EXISTS `inventorymanagementdb`.`staff` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NULL DEFAULT NULL,
+  `picture` VARCHAR(255) NULL DEFAULT NULL,
+  `bio` VARCHAR(2000) NULL DEFAULT NULL,
+  `position` VARCHAR(255) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 3
+DEFAULT CHARACTER SET = utf8;
+
+CREATE UNIQUE INDEX `id_UNIQUE` ON `inventorymanagementdb`.`staff` (`id` ASC);
 
 SET SQL_MODE = '';
 GRANT USAGE ON *.* TO admin@localhost;
@@ -140,32 +151,8 @@ SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `inventorymanagementdb`;
-INSERT INTO `inventorymanagementdb`.`customer` (`id`, `name`, `email`, `password`, `shipping_address`, `billing_address`, `cart_id`) VALUES (1, 'aaron', 'a@a.com', '123456', '123 main st.', '321 niam st.', 1);
-INSERT INTO `inventorymanagementdb`.`customer` (`id`, `name`, `email`, `password`, `shipping_address`, `billing_address`, `cart_id`) VALUES (2, 'bob', 'b@b.com', '123456', '456 elm st.', '654 mle st.', 2);
-
-COMMIT;
-
-
--- -----------------------------------------------------
--- Data for table `inventorymanagementdb`.`product`
--- -----------------------------------------------------
-START TRANSACTION;
-USE `inventorymanagementdb`;
-INSERT INTO `inventorymanagementdb`.`product` (`id`, `name`, `quantity`, `available`, `price`) VALUES (1, 'Coffee Mug', 20, true, 10.50);
-INSERT INTO `inventorymanagementdb`.`product` (`id`, `name`, `quantity`, `available`, `price`) VALUES (2, 'MacBookPro', 30, true, 3000.00);
-INSERT INTO `inventorymanagementdb`.`product` (`id`, `name`, `quantity`, `available`, `price`) VALUES (3, 'Sleeping Bag', 10, true, 75.30);
-
-COMMIT;
-
-
--- -----------------------------------------------------
--- Data for table `inventorymanagementdb`.`staff`
--- -----------------------------------------------------
-START TRANSACTION;
-USE `inventorymanagementdb`;
-INSERT INTO `inventorymanagementdb`.`staff` (`id`, `name`, `picture`, `bio`, `position`) VALUES (1, 'Tim', NULL, 'he does great things', 'manager of things');
-INSERT INTO `inventorymanagementdb`.`staff` (`id`, `name`, `picture`, `bio`, `position`) VALUES (2, 'Joe', NULL, 'he tries his best', 'assitant sorter');
-INSERT INTO `inventorymanagementdb`.`staff` (`id`, `name`, `picture`, `bio`, `position`) VALUES (3, 'Bill', NULL, 'he doesn\'t like spiders', 'executive accountant');
+INSERT INTO `inventorymanagementdb`.`customer` (`id`, `name`, `email`, `password`, `shipping_address`, `billing_address`) VALUES (1, 'adam', 'a@a.com', '123456', '123 main st.', '456 main st.');
+INSERT INTO `inventorymanagementdb`.`customer` (`id`, `name`, `email`, `password`, `shipping_address`, `billing_address`) VALUES (2, 'bob', 'b@b.com', '123456', '321 main st.', '654 main st.');
 
 COMMIT;
 
@@ -182,6 +169,18 @@ COMMIT;
 
 
 -- -----------------------------------------------------
+-- Data for table `inventorymanagementdb`.`product`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `inventorymanagementdb`;
+INSERT INTO `inventorymanagementdb`.`product` (`id`, `name`, `quantity`, `available`, `price`) VALUES (1, 'laptop', 10, true, 1000.00);
+INSERT INTO `inventorymanagementdb`.`product` (`id`, `name`, `quantity`, `available`, `price`) VALUES (2, 'coffee mug', 20, true, 10.00);
+INSERT INTO `inventorymanagementdb`.`product` (`id`, `name`, `quantity`, `available`, `price`) VALUES (3, 'bag of coffee', 30, true, 15.00);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
 -- Data for table `inventorymanagementdb`.`order_product`
 -- -----------------------------------------------------
 START TRANSACTION;
@@ -192,5 +191,17 @@ INSERT INTO `inventorymanagementdb`.`order_product` (`id`, `order_id`, `product_
 INSERT INTO `inventorymanagementdb`.`order_product` (`id`, `order_id`, `product_id`) VALUES (4, 2, 1);
 INSERT INTO `inventorymanagementdb`.`order_product` (`id`, `order_id`, `product_id`) VALUES (5, 2, 2);
 INSERT INTO `inventorymanagementdb`.`order_product` (`id`, `order_id`, `product_id`) VALUES (6, 2, 3);
+
+COMMIT;
+
+
+-- -----------------------------------------------------
+-- Data for table `inventorymanagementdb`.`staff`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `inventorymanagementdb`;
+INSERT INTO `inventorymanagementdb`.`staff` (`id`, `name`, `picture`, `bio`, `position`) VALUES (1, 'jim', NULL, 'works hard', 'woker');
+INSERT INTO `inventorymanagementdb`.`staff` (`id`, `name`, `picture`, `bio`, `position`) VALUES (2, 'joe', NULL, 'does things', 'doer');
+INSERT INTO `inventorymanagementdb`.`staff` (`id`, `name`, `picture`, `bio`, `position`) VALUES (3, 'john', NULL, 'happy to help', 'happy guy');
 
 COMMIT;
